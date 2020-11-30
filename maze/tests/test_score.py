37f37940cc5600df5d8ce7e3e8cd6e300591b109
oli817 <63024938+oli817@users.noreply.github.com>
2020-11-29 20:50:22 -0800

    Test for score

---
+++ maze/tests/test_score.py
@@ -0,0 +1,21 @@
+from models.score import Score
+import pytest
+
+@pytest.fixture
+def new_player():
+    """Fixturen for a new player"""
+    return Score(player_name="Rico", score=80, date="Sun Nov 29 15:59:51 2020")
+
+
+def test_score_attributes(new_player):
+    """Check the class has the correct attributs"""
+    assert new_player.player_name == "Rico"
+    assert new_player.score == 80
+    assert new_player.date == "Sun Nov 29 15:59:51 2020"
+
+
+def test_score_list_append(new_player):
+    """Check score list append corectlly"""
+    assert new_player.score_list == [{"player_name": "Rico", "score": 80, "date": "Sun Nov 29 15:59:51 2020"}]
+    assert len(new_player.score_list) == 1
+
