31391f3d24e01bd24a973caf4b4a0491cd6a2465
ethanloseweight <62718132+ethanloseweight@users.noreply.github.com>
2020-11-29 18:27:55 -0800

    Create web_api.py

---
+++ maze/web_api.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from maze.models.score_manager import ScoreManager
+
+app = Flask(__name__)
+
+score_manager = ScoreManager()
+score_manager.load_from_json()
+
+
+@app.route('/')
+def list_all_products():
+    return render_template("player_rank.html", players=score_manager.items)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
